f341e95bef190513c01717a75de82e6d87206db0
권진우 <wlsdn110@gmail.com>
2020-02-18 19:10:48 +0900

    req

--- routes/followReq.js
+++ routes/followReq.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/followreq', function(req, res, next) {
+router.get('/followReq', function(req, res, next) {
   token_values=nJwt.verify(req.headers.authorization,'nodebird', 'HS256');
   follower=token_values.body.id;
   following=req.body.following;
