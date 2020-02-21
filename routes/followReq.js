3b9fd3cc718851e30918ff471f56f698d55b1c74
jinwoo <wlsdn110@gmail.com>
2020-02-21 21:50:01 +0900

    .

--- routes/followReq.js
+++ routes/followReq.js
@@ -1,7 +1,8 @@
 var express = require('express');
 var router = express.Router();
 var nJwt = require('njwt');
-const { Follow,User } = require('../models');
+const { Follow} = require('../models');
+const {User} = require('../models');
 
 /* GET users listing. */
 router.get('/followReq', function(req, res, next) {
@@ -19,22 +20,13 @@ router.post('/search', function(req, res, next) {
   //token_values=nJwt.verify(req.headers.authorization,'nodebird', 'HS256');
   //follower=token_values.body.id;
   //following=req.body.following;
-  Follow.findAll({
-    where:{
-      nickname:{
-        [Op.like]:"%"+res.body.nickname+"%"
-      }
-    }
+  console.log("open");
+  var email='nova'
+  User.findOne({where:{nickname:{[Op.like]:'%'+email+'%'}}
   })
   .then(result=>{
-    res.json(result)
-  })
-  .catch(err=>{
-    console.log(err);
-    res.send(err);
+    res.json(JSON.stringify(result));
   })
 });
 
 module.exports = router;
-
-router.get
