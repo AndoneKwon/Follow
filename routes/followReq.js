062ecebb1b561c029bb6e4503238bbf7870961e0
jinwoo <wlsdn110@gmail.com>
2020-03-31 22:21:47 +0900

    final

--- routes/followReq.js
+++ routes/followReq.js
@@ -5,6 +5,7 @@ const { Follow,User } = require('../models');
 const Sequelize = require('sequelize');
 const {like} = Sequelize.Op
 var dotenv = require('dotenv').config();
+const axios=require('axios');
 
 var isEmpty = function(value){ 
   if( value == "" || value == null || value == undefined || ( value != null && typeof value == "object" && !Object.keys(value).length ) ){ 
@@ -26,7 +27,7 @@ router.post('/following',async function(req, res, next) {
   var following;
   var findResult;
   var isFollowed;
-
+  var notiBody={};
   await User.findAll({
     where:{
       nickname:followingNickName      
@@ -36,7 +37,9 @@ router.post('/following',async function(req, res, next) {
   .then(result=>{
     following=result[0].id;
   });//nickname에 맞는 ID를 찾아옴
-
+  console.log(following);
+  notiBody.send_user=token_values.body.nickname;
+  notiBody.rec_user=following;
   await Follow.findAll({
     where:{
       followerId:follower,
@@ -45,7 +48,7 @@ router.post('/following',async function(req, res, next) {
     paranoid:false//deleteAt 무시
   })
   .then(result=>{
-    console.log(result);
+    //console.log(result);
     findResult=result;
   })
   .catch(err=>{
@@ -62,7 +65,8 @@ router.post('/following',async function(req, res, next) {
       followerId:follower,
       followingId:following,
     })
-    .then(result=>{
+    .then(async result=>{
+      await axios.post("http://localhost:3006/noti/follow",notiBody);
       res.json({
         code:200,
         message : "following sueccess"
@@ -84,7 +88,8 @@ router.post('/following',async function(req, res, next) {
           followingId:following
         }
       })
-      .then(result=>{
+      .then(async result=>{
+        await axios.post("http://localhost:3006/noti/follow",notiBody);
         res.json({
           code:200,
           message : "following sueccess"
@@ -112,7 +117,8 @@ router.post('/unFollowing',async function(req, res, next) {
   var token_values=nJwt.verify(req.headers.authorization,process.env.JWT_SECRET, 'HS256');
   var follower=token_values.body.id;
   var followingNickName=req.body.nickname;
-
+  var notiBody={};
+  var following;
   /* 닉네임에 해당하는 id 불러옴 */
   await User.findAll({
     where:{
@@ -122,7 +128,8 @@ router.post('/unFollowing',async function(req, res, next) {
   .then(result=>{
     following=result[0].id;
   })
-
+  notiBody.send_user=follower;
+  notiBody.req_user=following;
   /* 팔로우 했는지 안했는지 여부 체크 */
   await Follow.findAll({
     where:{
@@ -148,13 +155,13 @@ router.post('/unFollowing',async function(req, res, next) {
     })
   }
 
-
  /* deletedAt 컬럼에 시간표시*/
   await Follow.destroy({where:{
     followerId:follower,
     followingId:following
   }})
   .then(result=>{
+    axios("117.17.196.142:3006/noti/unfollow",notiBody);
     res.json({
       code:200,
       message:'unFollow success'
@@ -185,6 +192,59 @@ router.post('/search', function(req, res, next) {
     var inform=result;
     res.json(inform);
   })//nick에 해당하는 nickname을 포함하고 있는 User들을 반환해줌.
+});
+
+router.get('/showFollowing', function(req, res, next) {
+  var token_values=nJwt.verify(req.headers.authorization,process.env.JWT_SECRET, 'HS256');
+  var myId = token_values.body.id;
+  var nick=req.body.nickname;
+  var nicknameList;
+  Follow.findAll({
+    where:{followerId:myId},
+    attributes:['followingId']
+  })
+  .then(result=>{
+    //console.log(result.nickname);
+    var inform=[];
+    //console.log()
+    for(let i=0;i<Object.keys(result).length;i++){
+      inform.push(result[i].followingId);
+    }
+    console.log(inform);
+    User.findAll({
+      where:{id:inform},
+      attributes:['nickname']
+    })
+    .then(result=>{
+      //console.log(result);
+      res.json(result);
+    })
+  })//내가 팔로우 하고있는 유저
+});
+
+router.get('/showFollower', function(req, res, next) {
+  var token_values=nJwt.verify(req.headers.authorization,process.env.JWT_SECRET, 'HS256');
+  var myId = token_values.body.id;
+  var nick=req.body.nickname;
+  Follow.findAll({
+    where:{followingId:myId},
+    attributes:['followerId']
+  })
+  .then(result=>{
+    //console.log(result.nickname);
+    var inform=[];
+    for(let i=0;i<Object.keys(result).length;i++){
+      inform.push(result[i].followerId);
+    }
+    User.findAll({
+      where:{id:inform},
+      attributes:['nickname']
+    })
+    .then(result=>{
+      //console.log(result);
+      res.json(result);
+    })
+  })//내가 팔로우 하고있는 유저
 });
 
 module.exports = router;
